2e308952fd05b4d4faeae9425ad1367fbd962c64
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:34:31 +0300

    add ru locale

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,1 +1,65 @@
-export default {};
+export default {
+  translation: {
+    login: {
+      username: 'Ваш ник',
+      password: 'Пароль',
+      logout: 'Выйти',
+      submit: 'Войти',
+      signup: 'Регистрация',
+      newToChat: 'Нет аккаунта?',
+      authFailed: 'Неверные имя пользователя или пароль',
+      header: 'Войти',
+      sсhemaRequired: 'Заполните все поля!',
+    },
+    notFound: {
+      pageNotFound: 'Страница не найдена',
+      transition: 'Но вы можете перейти',
+      mainPage: ' на главную страницу',
+    },
+    chatPage: {
+      channels: 'Каналы',
+      inputMessage: 'Введите сообщение...',
+      send: 'Отправить',
+      label: 'Новое сообщение',
+    },
+    modal: {
+      addChannel: 'Добавить канал',
+      channelName: 'Имя канала',
+      close: 'Отменить',
+      add: 'Отправить',
+      removeChannel: 'Удалить канал',
+      removeQuestion: 'Вы действительно хотите удалить канал?',
+      remove: 'Удалить',
+      rename: 'Переименовать канал',
+    },
+    errors: {
+      required: 'Заполните поле',
+      twiceChannels: 'Канал с таким именем уже существует',
+    },
+    dropMenu: {
+      menu: 'Управление каналом',
+      rename: 'Переименовать',
+      delete: 'Удалить',
+    },
+    signUp: {
+      registration: 'Регистрация',
+      minPass: 'Не менее 6 символов',
+      minName: 'От 3 до 20 символов',
+      maxName: 'От 3 до 20 символов',
+      difPass: 'Пароли должны совпадать',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      confirmPassword: 'Подтвердите пароль',
+      submit: 'Зарегистирироваться',
+      required: 'Заполните поле',
+      sameUsers: 'Пользователь с таким именем уже существует!',
+    },
+    toastify: {
+      unknowErr: 'Неизвестная ошибка!',
+      networkErr: 'Ошибка сети! Попробуйте позже.',
+      createChannel: 'Канал создан',
+      renameChannel: 'Канал переименован',
+      deleteChannel: 'Канал удалён',
+    },
+  },
+};
