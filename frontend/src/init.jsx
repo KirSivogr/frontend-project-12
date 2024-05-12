5fd7f8d3239be02cc88347f2d69fb596a26f22f0
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:35:48 +0300

    add websocket api

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,28 +1,34 @@
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/app/App';
 import resources from './locales/index.js';
-import store from './slices/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './slices/index.js';
+import { ApiContext } from './contexts/ApiContext';
+import './index.css';
+import webSocketApi from './websocket/websocket';
 
-const init = async () => {
-    const i18n = i18next.createInstance();
+const init = async (socket) => {
+  const api = webSocketApi(socket);
 
-    await i18n
-        .use(initReactI18next)
-        .init({
-            resources,
-            fallbackLng: 'ru',
-        });
+  const i18n = i18next.createInstance();
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
 
-    return (
-        <Provider store={store}>
-            <I18nextProvider i18n={i18n}>
-                <App />
-            </I18nextProvider>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <ApiContext.Provider value={api}>
+          <App />
+        </ApiContext.Provider>
+      </I18nextProvider>
+    </Provider>
+  );
 };
 
 export default init;
