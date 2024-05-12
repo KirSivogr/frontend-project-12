ba86a946a27e4d23530398fada45443d54035bf0
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:37:43 +0300

    add slices to store

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import channelReducer from './chanelSlice';
+import channelReducer from './channelSlice';
+import messagesReducer from './messageSlice';
+import modalReducer from './modalSlice';
 
 export default configureStore({
-    reducer: {
-        channel: channelReducer,
-    },
+  reducer: {
+    channel: channelReducer,
+    messages: messagesReducer,
+    modal: modalReducer,
+  },
 });
