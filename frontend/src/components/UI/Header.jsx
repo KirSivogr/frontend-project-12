de8f298413b1cedc586d1a6903bce2b540a6aed4
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:37:30 +0300

    add translation

--- frontend/src/components/UI/Header.jsx
+++ frontend/src/components/UI/Header.jsx
@@ -1,12 +1,17 @@
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useAuth } from '../../hooks/useAuth';
 
 export default () => {
-    return (
-        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-            <div className="container">
-                <Link to="/" className="navbar-brand">Chat</Link>
-                <button type="button" className="btn btn-primary">Выйти</button>
-            </div>
-        </nav>
-    );
+  const { t } = useTranslation();
+  const auth = useAuth();
+
+  return (
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light" style={{ background: '#f0f4fd' }}>
+      <div className="container">
+        <Link to="/" className="navbar-brand">Chat</Link>
+        {auth.user ? <button onClick={auth.logOut} type="button" className="btn btn-primary">{t('login.logout')}</button> : null}
+      </div>
+    </nav>
+  );
 };
