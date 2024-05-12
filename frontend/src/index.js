e061b1e24a2ec44d499a3f56d69629527ffef8fc
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:37:59 +0300

    connect socket io

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import { io } from 'socket.io-client';
 import init from './init.jsx';
 
 const app = async () => {
-    const root = ReactDOM.createRoot(document.getElementById('chat'));
-    root.render(await init());
+  const socket = io();
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
+  root.render(await init(socket));
 };
 
 app();
