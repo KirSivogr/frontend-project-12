1f652a7b2462ba36f0e54722d4b055a72fd00345
sivog <sivograkovkirill@mail.ru>
2024-05-12 14:37:09 +0300

    create auth hook

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
-
 import AuthContext from '../contexts/AuthContext';
 
 const useAuth = () => useContext(AuthContext);
 
-export default useAuth;
+export { useAuth };
